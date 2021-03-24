0b0918868c7391fb5efbac5167d013df0aaf963b
coltyngregory <coltyngregory@users.noreply.github.com>
2021-03-23 23:57:10 -0700

    Array Displaying
    Randomly generated array is now displaying.

--- sorting_visualizer/src/sortingVisualizer/sortingVisualizer.jsx
+++ sorting_visualizer/src/sortingVisualizer/sortingVisualizer.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-//import '.sortingVisualizer.css';
+import './sortingVisualizer.css';
 
 export class SortingVisualizer extends React.Component {
 	constructor(props) {
@@ -19,6 +19,7 @@ export class SortingVisualizer extends React.Component {
 			array.push(this.randomInt());
 		}
 		this.setState({array});
+		console.log(this.state.array);
 	}
 
 	randomInt() {
@@ -30,6 +31,14 @@ export class SortingVisualizer extends React.Component {
 
 		return(
 			<div className="array-container">
+			{array.map((value, idx) => (
+				<div
+				className="array-bar"
+				key={idx}
+				style={{height: `${value}px`}}>
+				</div>
+				))}
+			<br/>
 			<button onClick={() => this.resetArray()}>Generate New Array</button>
 			</div>
 			);
