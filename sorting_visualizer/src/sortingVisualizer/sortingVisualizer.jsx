e3c2dc51f575f9b59a285df5fb3b3192fcde4665
coltyngregory <coltyngregory@users.noreply.github.com>
2021-03-27 16:33:46 -0700

    fixed bug
    Array bars didn't revert back to original colour after turning green.
    
    Now they do.

--- sorting_visualizer/src/sortingVisualizer/sortingVisualizer.jsx
+++ sorting_visualizer/src/sortingVisualizer/sortingVisualizer.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
+import { bubbleSortAnimations } from './sortingAlgorithms.js';
 import './sortingVisualizer.css';
+
+const ANIMATIONS_SPEED_MS = 1;
 
 export class SortingVisualizer extends React.Component {
 	constructor(props) {
@@ -14,6 +17,8 @@ export class SortingVisualizer extends React.Component {
 	}
 
 	resetArray() {
+		this.makeAllBarsRed();
+		
 		const array = [];
 		for (let i = 0; i < 99; i++) {
 			array.push(this.randomInt());
@@ -24,6 +29,62 @@ export class SortingVisualizer extends React.Component {
 
 	randomInt() {
 		return Math.floor((Math.random() * 400) + 1);
+	}
+
+	bubbleSort() {
+		const array = this.state.array;
+		const animations = bubbleSortAnimations(array);
+		const bars = document.getElementsByClassName("array-bar");
+
+		for (let i = 0; i < animations.length; i++) {
+			setTimeout(() => {
+				var [oldPosition, newPosition] = animations[i];
+				var oldBarStyle = bars[oldPosition].style;
+				var newBarStyle = bars[newPosition].style;
+
+				var temp = this.state.array[oldPosition];
+				this.state.array[oldPosition] = this.state.array[newPosition];
+				this.state.array[newPosition] = temp;
+
+				oldBarStyle.height = `${this.state.array[oldPosition]}px`;
+				newBarStyle.height = `${this.state.array[newPosition]}px`;
+
+				oldBarStyle.backgroundColor = "blue";
+				newBarStyle.backgroundColor = "yellow";
+
+				var currentPosition = oldPosition;
+				for (let j = 0; j < currentPosition; j++) {
+					var jbar = bars[j].style;
+					jbar.backgroundColor = "blue";
+				}
+				if (i === animations.length - 1) {
+					this.makeAllBarsGreen();
+				}
+
+			}, i * ANIMATIONS_SPEED_MS);
+		}
+	}
+
+	makeAllBarsGreen() {
+		console.log("Sorted");
+		const arrayBars = document.getElementsByClassName("array-bar");
+		var arrayLength = arrayBars.length;
+
+		for (let j = 0; j < arrayLength; j++) {
+			var jBarStyle = arrayBars[j].style;
+			jBarStyle.backgroundColor = "limegreen";
+		}
+	}
+
+	makeAllBarsRed() {
+		console.log("Sorted");
+		const arrayBars = document.getElementsByClassName("array-bar");
+		var arrayLength = arrayBars.length;
+
+		for (let j = 0; j < arrayLength; j++) {
+			var jBarStyle = arrayBars[j].style;
+			jBarStyle.backgroundColor = "red";
+		}
 	}
 
 	render() {
@@ -40,6 +101,7 @@ export class SortingVisualizer extends React.Component {
 				))}
 			<br/>
 			<button onClick={() => this.resetArray()}>Generate New Array</button>
+			<button onClick={() => this.bubbleSort()}>Bubble Sort this whore</button>
 			</div>
 			);
 	}
