e25a1dc0a2d6a1e05ae46003b3c14ce0015f176f
coltyngregory <68095960+coltyngregory@users.noreply.github.com>
2021-04-07 23:48:07 -0700

    FireAudio
    FireAudio component is setup and working properly.

--- sorting_visualizer/src/components/fireAudio.js
+++ sorting_visualizer/src/components/fireAudio.js
@@ -15,9 +15,9 @@ class FireAudio extends React.Component {
 
 	render() {
 		return (
-			<div >
-			<audio loop="loop" id="audio" src="fire1.mp3" type="audio/mpeg" />
-			</div >
+			<div>
+			<audio loop="loop" id="audio" src="fire.mp3" type="audio/mpeg" />
+			</div>
 		);
 	};
 }
